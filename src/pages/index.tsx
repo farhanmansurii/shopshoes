abe2d932b45e0465ec8de5a21ffb7887ecfb9147
Farhan <mansurifarhanfm@gmail.com>
2023-04-18 04:41:16 +0530

    fixedd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,17 +32,19 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="mx-auto max-w-screen-xl min-h-screen px-4 sm:px-6 pt-[6rem] lg:px-8">
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className='text-4xl font-semibold py-5'>All Items</div>
+
+        <div className="grid grid-cols-2 pb-[10rem] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {listings?.map((product: Inputs) => (
             <Link key={product.id} href={`/${product?.id}`} className="flex flex-col border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transform transition duration-200 hover:scale-105">
               <div className=" pb-3/4">
                 <img src={product?.image} alt={product.name} className="  w-full h-full object-cover" />
               </div>
               <div className="px-4 py-2 flex-grow">
-                <h2 className="text-lg font-medium mb-2 capitalize">{product.name}</h2>
+                <h2 className="text-md lg:text-lg font-medium mb-2 capitalize">{product.name}</h2>
                 <div className="flex gap-2">
 
-                  <p className="text-gray-500 font-medium mb-2 ">₹{product.price}</p>
+                  <p className="text-gray-500 text-sm lg:text-md font-medium mb-2 ">₹{product.price}</p>
                   <p className={`text-xs mb-2 w-fit bg-green-100 px-3 py-1 capitalize rounded-xl font-medium text-green-500`}>
                     {product?.condition}
                   </p> </div>
--- src/pages/offers.tsx
+++ src/pages/offers.tsx
@@ -22,11 +22,11 @@ export default function Offers() {
   }
   const allMessage = messages?.flatMap((item) => item.message)
   return (
-    <div className='pt-[6rem] min-h-screen flex flex-col '>
-      <div className='text-4xl font-semibold px-7 lg:px-12 p-4'>All Offers</div>
-      {allMessage?.map((message) =>
+    <div className='pt-[6rem]  mb-10 min-h-screen flex flex-col '>
+      <div className='text-4xl font-semibold px-7  lg:px-12 p-4'>All Offers</div>
+      {allMessage?.map((message, i) =>
 
-        <div className=' flex gap-5 items-center px-7 lg:px-10 p-5 ' key={message?.id}>
+        <div className={` flex gap-5 items-center px-7 lg:px-10 p-5   ${i % 2 === 0 ? ' bg-blue-50/50' : ''}`} key={message?.id}>
           <Avatar />
           <div className='flex-col flex text-left'>
             <div className='font-semibold text-lg'>{message.fromUserName}</div>
